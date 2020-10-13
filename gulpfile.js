40ed4ce3684e8cf9e063107b86568471fea57a62
Carolina Lagerberg <carolina.lagerberg@live.se>
2020-10-13 19:20:03 +0200

    Fixed gulpfile and layout desktop

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var rename = require('gulp-rename');
 var csso = require("gulp-csso");
 
-gulp.task("default", function () {
+gulp.task("scss-items", function () {
   return gulp
     .src("scss/*.scss")
     .pipe(sass())
     .pipe(gulp.dest("css"))
+    .pipe(rename({suffix: '.min'}))
     .pipe(csso())
-    .pipe(gulp.dest("dist"));
-  // .pipe(gulp.watch)
+    .pipe(gulp.dest("dist/css"));
+});
+
+gulp.task("watch-scss", function() {
+  gulp.watch("scss/*.scss", gulp.series("scss-items"));
+});
+
+gulp.task("default", gulp.series("scss-items", "watch-scss"), function() {
+
 });
